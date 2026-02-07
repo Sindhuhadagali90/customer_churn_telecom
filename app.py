770a58cd2e2b16acf4a854f04fdb53dbf9f24ce6
Sindhuhadagali90 <sindhuhadagali44@gmail.com>
2026-02-07 20:48:32 +0530

    Remove pip install statement for streamlit
    Removed pip install statement for streamlit.

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/11QYKCkteIwyB1aHhLvwKFdNioUX16XDM
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
